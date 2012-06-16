ffdbff3730b5de7d0ec5aa9bdd0e2a02a10a14a3
Misha Schoolov <mschoolov@gmail.com>
2012-06-16 15:40:47 -0700

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
+    @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -25,9 +25,25 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
 
   it { should be_valid }
+	it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(admin: true)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -51,7 +67,7 @@ describe User do
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
-      end      
+      end
     end
   end
 
@@ -61,7 +77,7 @@ describe User do
       addresses.each do |valid_address|
         @user.email = valid_address
         @user.should be_valid
-      end      
+      end
     end
   end
 
@@ -83,7 +99,7 @@ describe User do
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
-  end	
+  end
 
 	describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
